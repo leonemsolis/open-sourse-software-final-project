4422a5c2c4d80c44080325867cf5980c7ada6b1b
Ruslan J <leonemsolis@gmail.com>
2017-09-19 15:44:28 +0900

    Initial commit

---
+++ desktop/src/com/leonemsolis/main/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.leonemsolis.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.leonemsolis.main.MainGameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGameClass(), config);
+
+	}
+}
