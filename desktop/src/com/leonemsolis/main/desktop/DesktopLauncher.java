c863d9abb277372b3d6231617af8cbecf75471a3
Ruslan J <leonemsolis@gmail.com>
2017-10-10 17:17:05 +0900

    Local

--- desktop/src/com/leonemsolis/main/desktop/DesktopLauncher.java
+++ desktop/src/com/leonemsolis/main/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.leonemsolis.main.MainGameClass;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 360;
-		config.height = 640;
+		config.width = 400;
+		config.height = 700;
 		new LwjglApplication(new MainGameClass(), config);
 
 	}
