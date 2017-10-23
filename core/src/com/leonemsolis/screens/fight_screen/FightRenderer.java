b9ff0109da32650add8942110024f6c6ee1dac54
Ruslan J <leonemsolis@gmail.com>
2017-10-23 21:09:14 +0900

    Fighter's labels added

--- core/src/com/leonemsolis/screens/fight_screen/FightRenderer.java
+++ core/src/com/leonemsolis/screens/fight_screen/FightRenderer.java
@@ -6,7 +6,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.leonemsolis.main.MainGameClass;
 import com.leonemsolis.screens.blueprints.Renderer;
 
@@ -19,6 +21,8 @@ public class FightRenderer extends Renderer {
     private OrthographicCamera camera;
     private BitmapFont redFont;
 
+    private final Rectangle heroLabel, enemyLabel;
+
     public FightRenderer() {
         camera = new OrthographicCamera();
         camera.setToOrtho(true, MainGameClass.GAME_WIDTH, MainGameClass.GAME_HEIGHT);
@@ -27,13 +31,23 @@ public class FightRenderer extends Renderer {
         shape = new ShapeRenderer();
         shape.setProjectionMatrix(camera.combined);
 
+        batch = new SpriteBatch();
+        batch.setProjectionMatrix(camera.combined);
+
 
         redFont = new BitmapFont(true);
         redFont.setColor(Color.RED);
-        redFont.getData().setScale(10);
+        // Make font height about 20
+        redFont.getData().setScale(1.272727f);
 
         GlyphLayout layout = new GlyphLayout();
         layout.setText(redFont, "Hero");
+        heroLabel = new Rectangle(140 / 2 - layout.width / 2, 3, layout.width, layout.height);
+
+        layout.setText(redFont, "Enemy");
+        enemyLabel = new Rectangle(180 + 140 / 2 - layout.width / 2, 3, layout.width, layout.height);
+
+        Gdx.app.log("height", layout.height+"");
     }
 
     @Override
@@ -41,11 +55,16 @@ public class FightRenderer extends Renderer {
         Gdx.gl20.glClearColor(0, 0, 0, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         shape.begin(ShapeRenderer.ShapeType.Filled);
+            // With cover
             shape.setColor(Color.WHITE);
-            shape.rect(0, MainGameClass.MID_POINT - 280, MainGameClass.GAME_WIDTH, 360);
-//            redFont.draw()
+            shape.rect(0, MainGameClass.MID_POINT - 400, MainGameClass.GAME_WIDTH, 480);
             shape.setColor(Color.RED);
             shape.rect(0, MainGameClass.MID_POINT + 80, MainGameClass.GAME_WIDTH, 200);
         shape.end();
+
+        batch.begin();
+            redFont.draw(batch, "Hero", heroLabel.x, heroLabel.y);
+            redFont.draw(batch, "Enemy", enemyLabel.x, enemyLabel.y);
+        batch.end();
     }
 }
