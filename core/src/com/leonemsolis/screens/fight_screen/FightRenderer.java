3084c306b28c733bb06782e4cfe80eb82fc5fd87
Ruslan J <leonemsolis@gmail.com>
2017-10-24 00:12:08 +0900

    Fight screen sketch added

--- core/src/com/leonemsolis/screens/fight_screen/FightRenderer.java
+++ core/src/com/leonemsolis/screens/fight_screen/FightRenderer.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.leonemsolis.main.MainGameClass;
 import com.leonemsolis.screens.blueprints.Renderer;
+import com.sun.org.apache.xpath.internal.operations.String;
 
 /**
  * Created by Leonemsolis on 10/10/2017.
@@ -19,9 +20,11 @@ import com.leonemsolis.screens.blueprints.Renderer;
 public class FightRenderer extends Renderer {
 
     private OrthographicCamera camera;
-    private BitmapFont redFont;
+    private BitmapFont blackFont;
 
-    private final Rectangle heroLabel, enemyLabel;
+    private final Rectangle heroLabel, enemyLabel, roundCounterLabel;
+
+    private int roundCounter = 1;
 
     public FightRenderer() {
         camera = new OrthographicCamera();
@@ -35,19 +38,20 @@ public class FightRenderer extends Renderer {
         batch.setProjectionMatrix(camera.combined);
 
 
-        redFont = new BitmapFont(true);
-        redFont.setColor(Color.RED);
+        blackFont = new BitmapFont(true);
+        blackFont.setColor(Color.BLACK);
         // Make font height about 20
-        redFont.getData().setScale(1.272727f);
+        blackFont.getData().setScale(1.272727f);
 
         GlyphLayout layout = new GlyphLayout();
-        layout.setText(redFont, "Hero");
-        heroLabel = new Rectangle(140 / 2 - layout.width / 2, 3, layout.width, layout.height);
+        layout.setText(blackFont, "Hero");
+        heroLabel = new Rectangle(10, 3, layout.width, layout.height);
 
-        layout.setText(redFont, "Enemy");
-        enemyLabel = new Rectangle(180 + 140 / 2 - layout.width / 2, 3, layout.width, layout.height);
+        layout.setText(blackFont, "Enemy");
+        enemyLabel = new Rectangle(MainGameClass.GAME_WIDTH - 10 - layout.width, 3, layout.width, layout.height);
 
-        Gdx.app.log("height", layout.height+"");
+        layout.setText(blackFont, roundCounter+"");
+        roundCounterLabel = new Rectangle(MainGameClass.GAME_WIDTH / 2 - layout.width / 2, 3, layout.width, layout.height);
     }
 
     @Override
@@ -60,11 +64,33 @@ public class FightRenderer extends Renderer {
             shape.rect(0, MainGameClass.MID_POINT - 400, MainGameClass.GAME_WIDTH, 480);
             shape.setColor(Color.RED);
             shape.rect(0, MainGameClass.MID_POINT + 80, MainGameClass.GAME_WIDTH, 200);
+
+            shape.rect(0, 23, MainGameClass.GAME_WIDTH / 2 - 10, 23);
+            shape.rect(MainGameClass.GAME_WIDTH / 2 + 20, 23, MainGameClass.GAME_WIDTH, 23);
+
+        shape.end();
+
+        shape.begin(ShapeRenderer.ShapeType.Line);
+            shape.setColor(Color.BLACK);
+
+            shape.rect(0, 23, MainGameClass.GAME_WIDTH / 2 - 10, 23);
+            shape.rect(MainGameClass.GAME_WIDTH / 2 + 20, 23, MainGameClass.GAME_WIDTH, 23);
+
+
+            shape.rect(MainGameClass.GAME_WIDTH / 2 - 20, 0, 40, 23);
+            shape.line(0, 23, MainGameClass.GAME_WIDTH, 23);
+
+
+            shape.line(0, 46, MainGameClass.GAME_WIDTH, 46);
+            shape.rect(MainGameClass.GAME_WIDTH / 2 - 1, 46, 2, 23);
+            shape.line(0, 69, MainGameClass.GAME_WIDTH, 69);
+
         shape.end();
 
         batch.begin();
-            redFont.draw(batch, "Hero", heroLabel.x, heroLabel.y);
-            redFont.draw(batch, "Enemy", enemyLabel.x, enemyLabel.y);
+            blackFont.draw(batch, "Hero", heroLabel.x, heroLabel.y);
+            blackFont.draw(batch, "Enemy", enemyLabel.x, enemyLabel.y);
+            blackFont.draw(batch, roundCounter+"", roundCounterLabel.x, roundCounterLabel.y);
         batch.end();
     }
 }
