f6a9f8c2368a21e6380b1123afec88157a9ed7bc
Ruslan J <leonemsolis@gmail.com>
2017-10-26 15:12:28 +0900

    Init commit

---
+++ core/src/com/leonemsolis/screens/map_screen/MapScreen.java
@@ -0,0 +1,10 @@
+package com.leonemsolis.screens.map_screen;
+
+import com.leonemsolis.screens.blueprints.Screen;
+
+/**
+ * Created by Leonemsolis on 26/10/2017.
+ */
+
+public class MapScreen extends Screen {
+}
