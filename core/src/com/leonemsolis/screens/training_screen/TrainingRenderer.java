cfecce54211dcb755a09592c1adbbce2377f8616
610ksh <610ksh@naver.com>
2017-11-23 18:45:27 +0900

    commit

--- core/src/com/leonemsolis/screens/training_screen/TrainingRenderer.java
+++ core/src/com/leonemsolis/screens/training_screen/TrainingRenderer.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.leonemsolis.main.MainGameClass;
+import com.leonemsolis.screens.common_objects.AssetHandler;
 
 /**
  * Created by kmusw on 2017-11-21.
@@ -16,8 +17,8 @@ import com.leonemsolis.main.MainGameClass;
 
 public class TrainingRenderer {
     private OrthographicCamera camera;
-//    private BitmapFont font;
-//    private SpriteBatch batch;
+    private BitmapFont font;
+    private SpriteBatch batch;
     private ShapeRenderer shape;
     public TrainingObjectHandler handler;
 
@@ -29,22 +30,30 @@ public class TrainingRenderer {
         shape = new ShapeRenderer(); // 모양을 가져올거야. 우리는 네모 Rectangle 직사각형
         shape.setProjectionMatrix(camera.combined); // 카메라에 쏜다.
         shape.setColor(Color.WHITE); // 파레트에다가 처음 색깔을 일단 저장한다.
+        batch = new SpriteBatch(); // 사진자료
+        batch.setProjectionMatrix(camera.combined);
+        font = new BitmapFont();
     }
 
     public void render(float delta) {
-        Gdx.gl20.glClearColor(0,0,0,1f); // 배경화면 색깔 gl20 이란, gl20이라는 openGL 그래픽 2.0 클래스를 뜻하는것
+        Gdx.gl20.glClearColor(255, 255, 255, 1f); // 배경화면 색깔 gl20 이란, gl20이라는 openGL 그래픽 2.0 클래스를 뜻하는것
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // gl도 마찬가지.
         shape.begin(ShapeRenderer.ShapeType.Filled); // 모양 그리기를 시작하는 함수이면서 동시에 어떤 방식으로 그릴지 (꽉찬 형태)
-            handler.select.render(shape); // 이게 experience 버튼을 생성하는 코드. // 경험버튼 클래스의 랜더함수를 사용.
-            handler.changeMap.render(shape);
-            handler.EXP.render(shape);
-            handler.SPD.render(shape);
-            handler.DEF.render(shape);
-            handler.ATK.render(shape);
+        handler.select.render(shape); // 이게 experience 버튼을 생성하는 코드. // 경험버튼 클래스의 랜더함수를 사용.
+        handler.changeMap.render(shape);
+        handler.EXP.render(shape);
+        handler.SPD.render(shape);
+        handler.DEF.render(shape);
+        handler.ATK.render(shape);
         shape.end(); // 모양그리기 종료
+        batch.begin();
+        batch.draw(AssetHandler.char_still[0], 20, MainGameClass.MID_POINT-100, 180, 220);
+        font.draw(batch,"Hello",MainGameClass.GAME_WIDTH,400);
+        batch.end();
     }
 
     public void dispose() {
         shape.dispose();
+        batch.dispose();
     }
 }
