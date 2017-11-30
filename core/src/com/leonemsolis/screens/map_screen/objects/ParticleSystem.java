7900f04c939aafcbc3ff77b4cd725eb0644b49ef
Ruslan J <leonemsolis@gmail.com>
2017-11-30 16:21:27 +0900

    Fixed Particle, Map Asset

--- core/src/com/leonemsolis/screens/map_screen/objects/ParticleSystem.java
+++ core/src/com/leonemsolis/screens/map_screen/objects/ParticleSystem.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Leonemsolis on 23/11/2017.
@@ -13,21 +14,20 @@ public class ParticleSystem {
     public int size = 500;
     public float ParticlePerSecond = 250;
 
-    public Vector2 velocity;
+    public float speed = 100f;
 
     public float ParticleLife = 4;
 
     public float x, y;
 
     public ArrayList<Particle>particles;
-
+    public Vector2 gravity;
     public boolean died = false;
 
     public ParticleSystem(float x, float y) {
         this.x = x;
         this.y = y;
-
-        velocity = new Vector2(10, 50);
+        gravity = new Vector2(0, 60);
 
         particles = new ArrayList<Particle>();
     }
@@ -35,9 +35,10 @@ public class ParticleSystem {
     public void update(float delta) {
         if(!died) {
             if(particles.size() < 500) {
+                Random random = new Random();
                 int numberOfParticleToCreate = (int)(ParticlePerSecond * delta);
                 for (int i = 0; i < numberOfParticleToCreate; ++i) {
-                    particles.add(new Particle(this));
+                    particles.add(new Particle(this, random.nextFloat() * 360));
                 }
             }
             died = true;
